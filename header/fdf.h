79d4ada59dae11ca41a6a28bf7e4065d1bebd300
Justin Owoundi Nlate <jowoundi@mi-c1-p2.s19.be>
2025-02-07 14:20:32 +0100

    save

--- header/fdf.h
+++ header/fdf.h
@@ -6,7 +6,7 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:34:21 by jowoundi          #+#    #+#             */
-/*   Updated: 2025/02/05 17:41:12 by jowoundi         ###   ########.fr       */
+/*   Updated: 2025/02/06 16:31:56 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,17 @@
 
 typedef struct s_mlx_data
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	void	*mlx;
+	void	*win;
 }		t_data;
 
+typedef struct s_img_data
+{
+	void	*img;
+	char	*address;
+	int		bits_per_pixel;
+	int		line_lenght;
+	int		endian;
+}		t_img_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jowoundi <jowoundi@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:01:24 by jowoundi          #+#    #+#             */
-/*   Updated: 2025/02/05 17:49:50 by jowoundi         ###   ########.fr       */
+/*   Updated: 2025/02/07 14:17:21 by jowoundi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,42 @@
 
 int	handle_intput(int keysym, t_data *data)
 {
-	if(keysym == XK_Escape)
+	if (keysym == XK_Escape)
 	{
 		ft_printf("the %d key (ESC) has been pressed \n\n", keysym);
-		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		mlx_destroy_display(data->mlx_ptr);
+		mlx_destroy_window(data->mlx, data->win);
+		mlx_destroy_display(data->mlx);
 		exit(1);
 	}
 	ft_printf("the %d key has been pressed\n\n", keysym);
 	return (0);
 }
 
-int main(void)
+void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color)
 {
-	t_data	data;
+	char	*dst;
+	
+	dst = data->address + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
 
-	data.mlx_ptr = mlx_init();
-	if (!data.mlx_ptr)
+int	main(void)
+{
+	t_data		data;
+	t_img_data	img;
+
+	data.mlx = mlx_init();
+	if (!data.mlx)
 		return (0);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WIDTH, HEIGHT, "Fil De Fer");
-	if (!data.win_ptr)
+	img.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
+	data.win = mlx_new_window(data.mlx, WIDTH, HEIGHT, "Fil De Fer");
+	if (!data.win)
 	{
-		mlx_destroy_display(data.mlx_ptr);
-		free(data.mlx_ptr);
+		mlx_destroy_display(data.mlx);
+		free(data.mlx);
 		return (0);
 	}
-	mlx_key_hook(data.win_ptr, handle_intput, &data);
-	mlx_loop(data.mlx_ptr);
+	img.address = mlx_get_data_addr(img.img, &img.bits_per_pixel, img.line_lenght, img.endian);
+	mlx_key_hook(data.win, handle_intput, &data);
+	mlx_loop(data.mlx);
 }
